698797089173df0008f8a3f6f0639bf051ea9b2e
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-08-17 01:45:46 +0300

    task_3

--- netflix/src/components/pages/mainPage/MainPage.js
+++ netflix/src/components/pages/mainPage/MainPage.js
@@ -2,17 +2,14 @@ import React, { Component } from 'react';
 import Banner from './banner';
 import MovieBoard from "./movieBoard";
 
-import './MainPage.scss'
-
 export default class MainPage extends Component {
-
-    render() {
-        return (
+   render() {
+      return (
        <>
             <Banner />
             <MovieBoard />
         </>
-        );
+       );
     }
 }
 
