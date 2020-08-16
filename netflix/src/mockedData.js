ef1d3e8e70304c1351f32494fa391c5236fa3664
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-08-17 01:38:58 +0300

    task_3

---
+++ netflix/src/mockedData.js
@@ -0,0 +1,46 @@
+ const movies = [
+    {
+      key: 'movie1',
+      src: 'https://i.ibb.co/YQvVhh2/img1.jpg',
+      title: 'title 1',
+      release: '2010',
+      category: 'Action & Drama',
+  },
+    {
+      key: 'movie2',
+      src: 'https://i.ibb.co/s3y6W9H/img2.jpg',
+      title: 'title 2',
+      release: '2011',
+      category: 'Action & Drama',
+    },
+    {
+      key: 'movie3',
+      src: 'https://i.ibb.co/yPN5ZX9/img3.jpg',
+      title: 'title 3',
+      release: '2015',
+      category: 'Action & Drama',
+    },
+    {
+      key: 'movie4',
+      src: 'https://i.ibb.co/s5ZNJxp/img4.jpg',
+      title: 'title 4',
+      release: '2017',
+      category: 'Action & Drama',
+    },
+    {
+      key: 'movie5',
+      src: 'https://i.ibb.co/4PxNgYB/img5.jpg',
+      title: 'title 5',
+      release: '2018',
+      category: 'Action & Drama',
+    },
+    {
+      key: 'movie6',
+      src: 'https://i.ibb.co/Fnz4Pr8/kinopoisk-ru.jpg',
+      title: 'title 6',
+      release: '2016',
+      category: 'Action & Drama',
+    },
+  ];
+
+  export default movies;
