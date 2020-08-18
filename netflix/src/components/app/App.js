047ee972333c9cdbecdbd544b4d4e8a1f15d6289
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-08-18 20:20:09 +0300

    task_3

--- netflix/src/components/app/App.js
+++ netflix/src/components/app/App.js
@@ -3,15 +3,13 @@ import MainPage from "../pages/mainPage";
 import Footer from '../shared/footer';
 import ErrorBoundary from '../errorBoundary';
 
-import './App.scss';
-
 const App = () => (
-  <div className="app">
+  <>
   <ErrorBoundary>
     <MainPage />
     <Footer />
   </ErrorBoundary>
-  </div>
+  </>
 );
 
 export default App;
