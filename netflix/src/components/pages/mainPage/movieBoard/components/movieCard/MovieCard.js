0ef6a62919ba9243861154b448f2dc6a117d38bb
Vladyslav Shypko <58210005+VladyslavShypko@users.noreply.github.com>
2020-08-18 23:22:40 +0300

    task_3

--- netflix/src/components/pages/mainPage/movieBoard/components/movieCard/MovieCard.js
+++ netflix/src/components/pages/mainPage/movieBoard/components/movieCard/MovieCard.js
@@ -5,17 +5,42 @@ import './MovieCard.scss'
 
 export default class MovieCard extends Component{
 
+  state = {
+    open: false,
+  }
+
+  openMovieActions = () => {
+      this.setState({
+        open: true
+      });
+  } 
+
+  closeMovieActions = () => {
+    this.setState({
+      open: false
+    });
+  }
+
   render() {
+
     const { src, title, release, category } = this.props;
+    const className = this.state.open ? "opened" : "closed";
+
     return (
      <div className='movie-card-wrapper'>
         <div className='movie-card'>
+           <div className='movie-card-menu' onClick={this.openMovieActions}></div>
             <img className='movie-poster' src={src}/>
             <div className='movie-title'>
                 <p className='movie-name'>{title}</p>
                 <p className='movie-release-date'>{release}</p>
             </div>
             <p className='movie-category'>{ category }</p>
+        </div>
+        <div className= {`modal-movie-actions ${className}`}>
+            <p className='close' onClick={this.closeMovieActions}>X</p>
+            <p>Edit</p>
+            <p>Delete</p>
         </div>
      </div>
     );
